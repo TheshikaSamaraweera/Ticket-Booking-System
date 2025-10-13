f3f665f1ed40888b76bb74f2b6cd9f8bf4aee5fb
TheshikaSamaraweera <theshikanavod@gmail.com>
2025-10-13 18:23:54 +0530

    initialize inventory service application structure

---
+++ inventryservice/src/main/java/com/democde/inventryservice/InventryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.democde.inventryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventryserviceApplication.class, args);
+	}
+
+}
---
+++ inventryservice/src/test/java/com/democde/inventryservice/InventryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.democde.inventryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
